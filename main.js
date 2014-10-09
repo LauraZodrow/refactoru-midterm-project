22795780111f3b66dc084f5dd3ff6c4254272a90
Laura <laura.zodrow@gmail.com>
2014-10-09 12:44:53 -0600

    working midterm project

--- main.js
+++ main.js
@@ -3,12 +3,84 @@ function initialize() {
 
   var markers = [];
   var map = new google.maps.Map(document.getElementById('map-canvas'), {
-    mapTypeId: google.maps.MapTypeId.ROADMAP
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    styles: [
+    {
+        "featureType": "road",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative.province",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "stylers": [
+            {
+                "color": "#004b76"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape.natural",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#fff6cb"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative.country",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#7f7d7a"
+            },
+            {
+                "lightness": 10
+            },
+            {
+                "weight": 1
+            }
+        ]
+    }
+]
   });
 
   var defaultBounds = new google.maps.LatLngBounds(
-      new google.maps.LatLng(-33.8902, 151.1759),
-      new google.maps.LatLng(-33.8474, 151.2631));
+      new google.maps.LatLng(-33.8902, 151.1759), 
+      new google.maps.LatLng(-33.8474, 151.2631)); 
   map.fitBounds(defaultBounds);
 
   // Create the search box and link it to the UI element.
@@ -76,38 +148,40 @@ function initialize() {
 google.maps.event.addDomListener(window, 'load', initialize);
 
 
+
+
 // =================================================
 // My Library Button
 // =================================================
 
-var Button = function(title) {
-	this.title = title;
-}
+// var Button = function(title) {
+// 	this.title = title;
+// }
 
-Button.prototype.render = function() {
-	if(this.element) return this.element;
+// Button.prototype.render = function() {
+// 	if(this.element) return this.element;
 
-	this.element = $('<div><h4>My Library</h4></div>');
+// 	this.element = $('<div><h4>My Library</h4></div>');
 
-	this.element 
-		.addClass('btn-library')
-		.appendTo('body');
+// 	this.element 
+// 		.addClass('btn-library')
+// 		.appendTo('body');
 
-	this.element.on('click', function () {
-		$('.my-library').slideDown(600);
-		$('.btn-library').hide();
-	});	
+// 	this.element.on('click', function () {
+// 		$('.my-library').slideDown(600);
+// 		$('.btn-library').hide();
+// 	});	
 
-	$('.my-library').on('click', 'h4', function() {
-		$('.my-library').slideUp(600);
-		$('.btn-library').show();
-	})
+// 	$('.my-library').on('click', 'h4', function() {
+// 		$('.my-library').slideUp(600);
+// 		$('.btn-library').show();
+// 	})
 
-	return this.element;
-};
+// 	return this.element;
+// };
 
-var btnLibrary = new Button('My Library');
-btnLibrary.render();
+// var btnLibrary = new Button('My Library');
+// btnLibrary.render();
 
 
 // =================================================
@@ -145,47 +219,97 @@ Location.prototype.render = function() {
    google.maps.event.addListener(this.marker, 'click', this.markerclick.bind(this));
 };
 
+//When Marker is clicked Modal appears, asks to add to Timeline.
+//If say yes, place name will be added to timeline
 Location.prototype.markerclick = function(e) {
-	console.log('clicked', this);
-	this.element = $('<div>Add to TravelLine</div>');
+	var placeName = this.marker.title;
+  // var locationDescription = $('.locationText').clone();
 
-	this.element
-		.addClass('addButton')
-		.appendTo(this.marker); //needs to be a dom element
+  $('#myModal').modal('show');
+  $('.timelineContainer').show();
 
-	return this.element;
+  $('.addButton').one('click', function () {
+      $('#myModal').modal('hide');
+
+
+      // var printDate = date.getYear() + ' ' + date.getDate();
+      var locationText = ('<textarea></textarea>');
+      var locationName = ('<h4>' + placeName + '</h4>');
+      var locationBlock = ('<div class="locationContainer">' + locationName + locationText + '</div>');
+
+
+      $('.timelineContainer').append(locationBlock);
+  });
+
+  console.log('clicked', this);
 };
 
-var addToTimeline = new Location();
-addToTimeline.render();
-
-
 // =================================================
-// Bookmark Board
+// Bookmark Board - Add Buttons
 // =================================================
 
-// var CustomBoard = function(title) {
-// 	this.title = title;
-// }
+$(document).on('click', '.addBtnBoard', function() {
+    $('.addBtnBoard li').slideToggle(600);
+}); 
 
-// var Schools = function (title) {
-// 	this.title = title;
-// }
+$(document).on ('click', '.addSchools', function() {
+    // if this.element return !this.element;
+    $('.schools').show();
+})
 
-// Schools.prototype.render = function() {
-// 	if(this.element) return this.element;
+$(document).on ('click', '.addRestaurants', function() {
+    // if this.element return !this.element;
+    $('.restaurants').show();
+})
 
-// 	this.element = $('<div><h4>Schools</h4</div>');
+$(document).on ('click', '.addHostels', function() {
+    // if this.element return !this.element;
+    $('.hostels').show();
+})
 
-// 	this.element 
-// 		.addClass('schoolBox')
-// 		.appendTo('body');
+$(document).on ('click', '.addMuseums', function() {
+    // if this.element return !this.element;
+    $('.museums').show();
+})
 
-// 	return this.element;
-// };
+$(document).on ('click', '.addPubs', function() {
+    // if this.element return !this.element;
+    $('.pubs').show();
+})
 
-// var mySchools = new Schools('Schools');
-// mySchools.render();
+// =================================================
+// Bookmark Board - Delete Buttons
+// =================================================
+
+$(document).on('click', '.btnBoardSchool', function(){
+    $('.btnBoardSchool').closest('.schools').hide();
+});
+
+$(document).on('click', '.btnBoardRestaurants', function(){
+    $('.btnBoardRestaurants').closest('.restaurants').hide();
+});
+
+$(document).on('click', '.btnBoardHostels', function(){
+    $('.btnBoardHostels').closest('.hostels').hide();
+});
+
+$(document).on('click', '.btnBoardMuseums', function(){
+    $('.btnBoardMuseums').closest('.museums').hide();
+});
+
+$(document).on('click', '.btnBoardPubs', function(){
+    $('.btnBoardPubs').closest('.pubs').hide();
+});
+
+$(document).on('click', '.btnBoardCreateOwn', function(){
+    $('.btnBoardCreateOwn').closest('.createOwn').hide();
+});
+
+var addBtnBoard = function() {
+
+}
+
+
 
 
 
